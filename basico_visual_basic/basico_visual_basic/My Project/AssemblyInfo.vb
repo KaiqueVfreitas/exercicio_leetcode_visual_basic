55842dfd2b440f0125fe2c5bdb30a202890c8484
Kaique Vieira <108439058+KaiqueVfreitas@users.noreply.github.com>
2025-04-22 14:30:36 -0300

    Primeiros comandos: Hello Word e Ola Mundo

---
+++ basico_visual_basic/basico_visual_basic/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("basico_visual_basic")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("basico_visual_basic")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("74100bc7-dfc9-4033-8c5b-41c9d1922d07")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ basico_visual_basic/basico_visual_basic/Soma.vb
@@ -0,0 +1,5 @@
+﻿Public Class Soma
+    Public Shared Function CalcularSoma(decNum1 As Decimal, decNum2 As Decimal) As Decimal
+        Return decNum1 + decNum2
+    End Function
+End Class
