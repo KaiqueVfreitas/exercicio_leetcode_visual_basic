14946b3537e5de98c34a19b4021d3f76182120ea
Kaique Vieira <108439058+KaiqueVfreitas@users.noreply.github.com>
2025-04-23 13:35:42 -0300

    Operador MOD que substitui o % no VB.NET

--- basico_visual_basic/basico_visual_basic/Module1.vb
+++ basico_visual_basic/basico_visual_basic/Module1.vb
@@ -1,20 +1,22 @@
 ﻿Module Module1
 
     Sub Main()
-        Dim decNum1 As Decimal
-        Dim decNum2 As Decimal
-        Dim decResult As Decimal
+        'Dim decNum1 As Decimal
+        'Dim decNum2 As Decimal
+        'Dim decResult As Decimal
 
-        Console.WriteLine("SOMA DOIS NÚMEROS")
-        Console.Write("Digite o numero 1:")
-        decNum1 = Console.ReadLine()
-        Console.Write("Digite o numero 2:")
-        decNum2 = Console.ReadLine()
+        'Console.WriteLine("SOMA DOIS NÚMEROS")
+        'Console.Write("Digite o numero 1:")
+        'decNum1 = Console.ReadLine()
+        'Console.Write("Digite o numero 2:")
+        'decNum2 = Console.ReadLine()
 
-        decResult = Soma.CalcularSoma(decNum1, decNum2)
+        'decResult = Soma.CalcularSoma(decNum1, decNum2)
 
 
-        Console.Write($"O seu resultado foi {decResult}")
+        'Console.Write($"O seu resultado foi {decResult}")
+
+        Console.WriteLine(OperadoresAritmeticos.ExpressaoPreDefinida)
 
     End Sub
 
---
+++ basico_visual_basic/basico_visual_basic/OperadoresAritmeticos.vb
@@ -0,0 +1,8 @@
+﻿Public Class OperadoresAritmeticos
+    Public Shared Function ExpressaoPreDefinida() As Decimal
+        Dim expressao As Decimal
+        expressao = (3 * 3 * (3 + 5) / (1 + 2) ^ (5 - 3) Mod 2)
+
+        Return expressao
+    End Function
+End Class
